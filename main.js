921473e4e9dc273bec076d725db8a6f5a431b1c6
SirPopper <48968500+SirPopper@users.noreply.github.com>
2022-12-02 18:42:46 +0900

    Added logic of all ops buttons
    Add functionality of operations buttons including clear and delete button.

--- main.js
+++ main.js
@@ -28,6 +28,7 @@ function divide(a, b) {
 }
 
 //making the functions work
+let opsArray = [];
 const displayUpper = document.querySelector(".display-upper");
 
 const keyOps = document.querySelectorAll(".key.ops");
@@ -35,5 +36,51 @@ keyOps.forEach((key) => {
   key.addEventListener("click", () => {
     displayUpper.textContent = displayLower.textContent;
     displayLower.textContent = "";
+    opsArray.push(key.textContent);
+    console.log(opsArray);
   });
+});
+
+//equal button logic
+const keyEqual = document.querySelector(".key.equal");
+
+keyEqual.addEventListener("click", () => {
+  console.log(opsArray.length);
+  if (opsArray[opsArray.length - 1] == "%") {
+    displayLower.textContent = divide(
+      Number(displayUpper.textContent),
+      Number(displayLower.textContent)
+    );
+  } else if (opsArray[opsArray.length - 1] == "x") {
+    displayLower.textContent = multiply(
+      Number(displayUpper.textContent),
+      Number(displayLower.textContent)
+    );
+  } else if (opsArray[opsArray.length - 1] == "-") {
+    displayLower.textContent = subtract(
+      Number(displayUpper.textContent),
+      Number(displayLower.textContent)
+    );
+  } else if (opsArray[opsArray.length - 1] == "+") {
+    displayLower.textContent = add(
+      Number(displayUpper.textContent),
+      Number(displayLower.textContent)
+    );
+  }
+});
+
+//clear button logic
+const clearBtn = document.querySelector(".btn-clear");
+
+clearBtn.addEventListener("click", () => {
+  displayLower.textContent = "";
+  displayUpper.textContent = "";
+});
+
+//delete button logic
+const delBtn = document.querySelector(".btn-delete");
+
+delBtn.addEventListener("click", () => {
+  let text = displayLower.textContent;
+  displayLower.textContent = text.slice(0, -1);
 });
